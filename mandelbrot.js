7f3b67f7eec32be7713a72eb41ac8ef957394507
starcolon <tao@starcolon.com>
2015-06-21 16:37:27 +0700

    MANDELBROT DRAW WITH OFFSET

--- mandelbrot.js
+++ mandelbrot.js
@@ -5,7 +5,7 @@
  * Dependencies: Raphaël.js
  */
 
-function render(size,bound,maxIter){
+function render(size,bound,center,maxIter){
 	console.log('bound = ' + parseFloat(bound).toFixed(3));
 
 	// Initialize the canvas
@@ -24,12 +24,12 @@ function render(size,bound,maxIter){
 	var operations = [];
 	var iter = 0;
 	console.log('sequencing ...');
-	for (var u=-size/2; u<size/2; u++)
-		for (var v=-size/2; v<size/2; v++){
-			var c = canvasCoordToPlaneCoord(u, v, bound, size);
+	for (var u=0; u<size; u++)
+		for (var v=0; v<size; v++){
+			var c = canvasCoordToPlaneCoord(u, v, center, bound, size);
 			// Generate Mandelbrot sequence from this coordinate
 			operations.push(new Promise(function(resolve,reject){
-				mandelbrot(c, bound, size, paper, maxIter);
+				mandelbrot(center, c, bound, size, paper, maxIter);
 				resolve();
 			}));
 		}
@@ -44,22 +44,22 @@ function render(size,bound,maxIter){
 }
 
 
-function canvasCoordToPlaneCoord(u,v,bound,size){
+function canvasCoordToPlaneCoord(u,v,center,bound,size){
 	return {
-		re: (u-(size/2))*bound/(size/2),
-		im: (v-(size/2))*bound/(size/2)
+		re: (u-(size/2))*bound/(size/2) + Re(center),
+		im: (v-(size/2))*bound/(size/2) + Im(center)
 	}
 }
 
-function planeCoordToCanvasCoord(z,bound,size){
+function planeCoordToCanvasCoord(z,center,bound,size){
 	var s = (size/2)/bound;
 	return {
-		x: size/2 + s*Re(z), 
-		y: size/2 + s*Im(z)
+		x: size/2 + s*(Re(z) - Re(center)), 
+		y: size/2 + s*(Im(z) - Im(center))
 	}
 }
 
-function mandelbrot(c,bound,size,paper,maxIter){
+function mandelbrot(center,c,bound,size,paper,maxIter){
 	var iter = 0;
 	var z = {re: 0, im: 0};
 	while (Abs(z)<bound && iter<maxIter){
@@ -71,15 +71,21 @@ function mandelbrot(c,bound,size,paper,maxIter){
 	// If the iteration does not exceed the maximum constraint,
 	// then it is a member of the set
 	if (iter<maxIter){
-		var coord = planeCoordToCanvasCoord(c, bound, size);
+		var coord = planeCoordToCanvasCoord(c, center, bound, size);
 		var intensity = 255 - (255*iter/maxIter);
 		var R = parseInt(intensity);
 		var G = parseInt(Math.ceil(intensity*0.25));
 		var B = parseInt(Math.ceil(intensity*0.3));
+		function showCoord(cx){
+			return function(event){
+				console.log('> ' + parseFloat(Re(cx)).toFixed(5) + ' : ' + parseFloat(Im(cx)).toFixed(5)+'i');
+			}
+		}
 		paper.circle(coord.x, coord.y, 1).attr({
 			fill: 'rgba('+R+','+G+','+B+',0.8)', 
 			stroke: 'none'
-		});
+		})
+		//.click(showCoord(c));
 	}
 }
 
