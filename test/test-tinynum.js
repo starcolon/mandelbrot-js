9e835b337f0492499e6e5ce6c87073bc83b2c1f9
starcolon <tao@starcolon.com>
2015-06-22 20:25:16 +0700

    WRITE TEST SCRIPT FOR {TINYNUM}

---
+++ test/test-tinynum.js
@@ -0,0 +1,51 @@
+/**
+ * TinyNum - tester
+ * ------------------------------------------------------
+ * This test suite can be executed on "mocha" only.
+ */
+
+var expect = require('expect');
+var TinyFloat = require('../lib/tinynum.js');
+
+//-------------------------------------------------------
+
+describe('tinynum module tests', function(){
+	describe('basic tinyfloat tests', function(){
+		it('should initialize the tiny num with appropriate notations', function(){
+			var v1 = new TinyFloat(1, -15);
+			var v2 = new TinyFloat(2.2, -16);
+			expect(TinyFloat.greater(v1,v1)).to.be.true;
+		})
+
+		it('should normalize a tiny float correctly', function(){
+			var v = new TinyFloat(88825, -20);
+			v.resolve();
+			expect(v.a).to.be.equal(8.8825);
+			expect(v.exp).to.be.equal(-16);
+		})
+
+		it('should add two tiny floats',function(){
+			var v1 = new TinyFloat(25, 0);
+			var v2 = new TinyFloat(111,1);
+			var v3 = TinyFloat.add(v1,v2);
+			expect(v3.a).to.be.equal(2.775);
+			expect(v3.exp).to.be.equal(4);
+		})
+
+		it('should multiply two tiny floats', function(){
+			var v1 = new TinyFloat(3, 8);
+			var v2 = new TinyFloat(2, -12);
+			var v3 = TinyFloat.mul(v1,v2);
+			expect(v3.a).to.be.equal(6);
+			expect(v3.exp).to.be.equal(-4);
+		})
+
+		it('should power a tiny float by 2', function(){
+			var v1 = new TinyFloat(0.00000025);
+			var v2 = TinyFloat.sqr(v1);
+			expect(v2.a).to.be.equal(6.25);
+			expect(v2.exp).to.be.equal(-14);
+		})
+
+	})
+})
